b2d95fb78465d3ed4482036d3e6eec4db0b91283
JooYoungHeo <jyhur900226@naver.com>
2017-10-29 21:26:18 +0900

    [Work] api -> call another api test

--- routes/callData.js
+++ routes/callData.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const request = require('request');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json([1,2,3]);
+router.post('/', (req, res) => {
+  let options = {
+    url: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
+    method: 'PUT'
+  };
+
+  let thisRes = res;
+
+  request(options, (err, res, body) => {
+    thisRes.json(JSON.parse(body));
+  });
 });
+
+router.put('/', (req, res) => {
+  res.json({
+    list: [4,5,6]
+  });
+})
 
 module.exports = router;
