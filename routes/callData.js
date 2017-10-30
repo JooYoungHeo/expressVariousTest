d6d3a7edee5edbd7d4c7fe1a19a408fd2a0f0d82
JooYoungHeo <jyhur900226@naver.com>
2017-10-30 10:16:24 +0900

    [Work] resend data to another router

--- routes/callData.js
+++ routes/callData.js
@@ -6,20 +6,22 @@ const router = express.Router();
 router.post('/', (req, res) => {
   let options = {
     url: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
-    method: 'PUT'
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json'
+    },
+    json: req.body
   };
 
   let thisRes = res;
 
   request(options, (err, res, body) => {
-    thisRes.json(JSON.parse(body));
+    thisRes.json(body);
   });
 });
 
 router.put('/', (req, res) => {
-  res.json({
-    list: [4,5,6]
-  });
-})
+  res.json(req.body);
+});
 
 module.exports = router;
