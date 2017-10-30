f80d0703f2af3c7e481c4754b86bd9daf49d1815
JooYoungHeo <jyhur900226@naver.com>
2017-10-30 09:59:01 +0900

    [Work] middleware order change

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ let api = require('./routes');
 let app = express();
 let port = 3000;
 
-app.use('/', api);
 app.use(bodyParser.json());
+app.use('/', api);
 
 app.listen(port, () => {
   console.log(`server start # ${port}`);
